4e364fb64f1899df687aabfd794dfe1bca990c3b
kevinleiton772 <kevin.leiton772@ist17dejulio.edu.ec>
2026-06-08 17:36:19 -0500

    Metodos con Argumento

--- src/mode/Cliente.java
+++ src/mode/Cliente.java
@@ -2,9 +2,28 @@
 package mode;
 
 public class Cliente {
+    //Atributos Globales
     String nombre;
     String apellido;
     int cedula;
     String telefono;
     String direccion;
+    
+    //Metodo contructor = sirve para inicializar los atributos i=10;
+    // Metodo contructor vacio y me permite personalizar los atributos que quiero inicializar 
+    public Cliente(){
+    
+        
+    }
+  
+   // Metodo contructor con argumnetos => variable local
+    public Cliente(String nombre,String apellido,int cedula,String telefono,String direccion){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.cedula=cedula;
+        this.telefono=telefono;
+        this.direccion=direccion;
+        
+    }
 }
+            
